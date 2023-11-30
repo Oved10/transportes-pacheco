aaa3a9ad94a65e8f0e71b77a4d57da44c9e59583
Oved10 <145045932+Oved10@users.noreply.github.com>
2023-11-30 09:25:03 -0600

    Update viajes.php

--- application/views/viajes.php
+++ application/views/viajes.php
@@ -135,7 +135,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
                   <div class="form-group col-sm-4">
                     <label for="" style="color: white" >Placa Del Camion</label>
-                    <input type="text" name="truck" class="form-control me-2 light-table-filter" id="truck" >
+                    <input type="text" name="camion" class="form-control me-2 light-table-filter" id="camion" >
 
                   </div> 
 
@@ -253,8 +253,38 @@ defined('BASEPATH') OR exit('No direct script access allowed');
          <script>
         $(document).ready(function(){
             $('#dataviaje').DataTable({
-                responsive: true
+
+                responsive: true,
+                lengthMenu: [5,6,10],
+                 columnDefs: [
+                { className: "centered", targets: [0, 1, 2, 3, 4, 5, 6] },
+                { orderable: false, targets: [5, 6] },
+                { searchable: false, targets: [1] }
+                //{ width: "50%", targets: [0] }
+            ],
+            pageLength: 3,
+             destroy: true,
+            language: {
+                lengthMenu: "Mostrar _MENU_ registros por página",
+                zeroRecords: "Ningún usuario encontrado",
+                info: "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+                infoEmpty: "Ningún usuario encontrado",
+                infoFiltered: "(filtrados desde _MAX_ registros totales)",
+                search: "Buscar:",
+                loadingRecords: "Cargando...",
+                paginate: {
+                    first: "Primero",
+                    last: "Último",
+                    next: "Siguiente",
+                    previous: "Anterior"
+                }
+            }
+
+
+
             });
+
+
         });
 
 
